9cac461780abc68a3d159bb0968794fc4921f9dc
JeffBuss <turbocapslock@gmail.com>
2017-01-04 20:27:04 -0700

    Add radio buttons

--- lib/components/Frequency.jsx
+++ lib/components/Frequency.jsx
@@ -2,34 +2,41 @@ import React from 'react';
 import { render } from 'react-dom';
 import firebase, { reference, signIn, signOut } from '../firebase.js';
 
-render() {
-  return (
-    <div>
-      <h1>Frequency</h1>
-      <input
-        type='radio'
-        placeholder='Daily'
-        value=''
-        onChange=''
-      />
-      <input
-        type='radio'
-        placeholder='Weekly'
-        value=''
-        onChange=''
-      />
-      <input
-        type='radio'
-        placeholder='Monthly'
-        value=''
-        onChange=''
-      />
-      <input
-        type='radio'
-        placeholder='Annually'
-        value=''
-        onChange=''
-      />
-    </div>
-  )
+export default class Frequency extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Frequency</h1>
+        <input
+          type='radio'
+          name='frequency radio'
+          placeholder='Daily'
+          value=''
+          onChange=''
+        />
+        <input
+          type='radio'
+          name='frequency radio'
+          placeholder='Weekly'
+          value=''
+          onChange=''
+        />
+        <input
+          type='radio'
+          name='frequency radio'
+          placeholder='Monthly'
+          value=''
+          onChange=''
+        />
+        <input
+          type='radio'
+          name='frequency radio'
+          placeholder='Annually'
+          value=''
+          onChange=''
+        />
+      </div>
+    )
+  }
 }
--- lib/components/Transactions.jsx
+++ lib/components/Transactions.jsx
@@ -1,36 +1,42 @@
 import React from 'react';
 import { render } from 'react-dom';
 import firebase, { reference, signIn, signOut } from '../firebase.js';
+import Frequency from './Frequency';
 
-render() {
-  return (
-    <div>
-      <header>Trapper Keeper</header>
-      <h1>Transactions</h1>
-      <input
-        type='text'
-        placeholder='The MF Thief'
-        value=''
-        onChange=''
-      />
-      <input
-        type='text'
-        placeholder='Amount'
-        value=''
-        onChange=''
-      />
-      <input
-        type='text'
-        placeholder='Date'
-        value=''
-        onChange=''
-      />
-      <input
-        type='radio'
-        placeholder='Recurring?'
-        value=''
-        onChange=''
-      />
-    </div>
-  )
+export default class Transactions extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <header>Trapper Keeper</header>
+        <h1>Transactions</h1>
+        <input
+          type='text'
+          placeholder='The MF Thief'
+          value=''
+          onChange=''
+        />
+        <input
+          type='text'
+          placeholder='Amount'
+          value=''
+          onChange=''
+        />
+        <input
+          type='text'
+          placeholder='Date'
+          value=''
+          onChange=''
+        />
+        <input
+          type='radio'
+          placeholder='Recurring?'
+          value=''
+          onChange=''
+        />
+        <Frequency
+        />
+      </div>
+    )
+  }
 }
