eb5601708a1621096b89e8828e5d8194e6bbcf45
Elijah Williams <ejwill04@gmail.com>
2017-01-04 19:55:44 -0700

    Break out login and out into component

--- lib/LogInOut.jsx
+++ lib/LogInOut.jsx
@@ -1,1 +1,30 @@
 import React from 'react';
+import { signIn, signOut } from './firebase.js';
+
+export const LogInOut = ({ user }) => {
+  if (user) {
+    return (
+      <div>
+        <p
+          className='user-greeting'>
+          Logged in as {user.displayName} ({user.email})
+        </p>
+        <button
+          className='sign-out'
+          onClick={() => signOut()}>
+          Sign Out
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <button
+          className='sign-in'
+          onClick={() => signIn()}>
+          Login
+        </button>
+      </div>
+    );
+  }
+};
