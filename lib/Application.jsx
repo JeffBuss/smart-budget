4c7c0576c66c8a70ba2fac5f669a75d0f666bdcf
chelle tuerk <michelletuerk@gmail.com>
2017-01-09 18:42:03 -0700

    no more refresh after login to show data

--- lib/Application.jsx
+++ lib/Application.jsx
@@ -37,7 +37,10 @@ export default class Application extends React.Component {
   componentDidMount() {
     this.getDataFromFirebase('content', 'content');
     this.getDataFromFirebase('funds', 'bankAccount');
-    firebase.auth().onAuthStateChanged(user => this.setState({ user }));
+    firebase.auth().onAuthStateChanged(user => this.setState({ user }, () => {
+      this.getDataFromFirebase('content', 'content');
+      this.getDataFromFirebase('funds', 'bankAccount');
+    }));
   }
 
   getDataFromFirebase(fbArray, state) {
